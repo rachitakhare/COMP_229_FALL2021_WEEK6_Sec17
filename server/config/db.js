67f06cc930f4753433c2b7c1bdc571411d156ea1
rachita <rachitakhare15@gmail.com>
2021-10-23 20:17:11 -0400

    week6--authentication sec17

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://rachita:dQ9w8UCe3iMrRNLQ@mongodatabaseservers17.ppcmu.mongodb.net/book_store?retryWrites=true&w=majority"
+}
